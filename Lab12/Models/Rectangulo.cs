e10f4eb791e3fea1651dbf3f02e7c3654ca35150
Eduardo Black <edublackf@gmail.com>
2023-06-16 22:36:32 -0500

    Agregar archivos de proyecto.

---
+++ Lab12/Models/Rectangulo.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Lab12.Models
+{
+    public class Rectangulo : INotifyPropertyChanged
+    {
+        private double baseRectangulo;
+        public double BaseRectangulo
+        {
+            get { return baseRectangulo; }
+            set
+            {
+                if (baseRectangulo != value)
+                {
+                    baseRectangulo = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private double alturaRectangulo;
+        public double AlturaRectangulo
+        {
+            get { return alturaRectangulo; }
+            set
+            {
+                if (alturaRectangulo != value)
+                {
+                    alturaRectangulo = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private double area;
+        public double Area
+        {
+            get { return area; }
+            set
+            {
+                if (area != value)
+                {
+                    area = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private double perimetro;
+        public double Perimetro
+        {
+            get { return perimetro; }
+            set
+            {
+                if (perimetro != value)
+                {
+                    perimetro = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Lab12/ViewModels/RectanguloViewModel.cs
@@ -0,0 +1,95 @@
+﻿using Lab12.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Lab12.ViewModels
+{
+    public class RectanguloViewModel : INotifyPropertyChanged
+    {
+        private Rectangulo rectangulo;
+
+        public double BaseRectangulo
+        {
+            get { return rectangulo.BaseRectangulo; }
+            set
+            {
+                if (rectangulo.BaseRectangulo != value)
+                {
+                    rectangulo.BaseRectangulo = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public double AlturaRectangulo
+        {
+            get { return rectangulo.AlturaRectangulo; }
+            set
+            {
+                if (rectangulo.AlturaRectangulo != value)
+                {
+                    rectangulo.AlturaRectangulo = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public double Area
+        {
+            get { return rectangulo.Area; }
+            set
+            {
+                if (rectangulo.Area != value)
+                {
+                    rectangulo.Area = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public double Perimetro
+        {
+            get { return rectangulo.Perimetro; }
+            set
+            {
+                if (rectangulo.Perimetro != value)
+                {
+                    rectangulo.Perimetro = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand CalcularAreaCommand { get; private set; }
+        public ICommand CalcularPerimetroCommand { get; private set; }
+
+        public RectanguloViewModel()
+        {
+            rectangulo = new Rectangulo();
+            CalcularAreaCommand = new Command(CalcularArea);
+            CalcularPerimetroCommand = new Command(CalcularPerimetro);
+        }
+
+        private void CalcularArea()
+        {
+            Area = BaseRectangulo * AlturaRectangulo;
+        }
+
+        private void CalcularPerimetro()
+        {
+            Perimetro = 2 * (BaseRectangulo + AlturaRectangulo);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
